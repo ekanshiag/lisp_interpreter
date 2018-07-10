29a17581afcc52f450389f1d8f6dff004bfaba32
Ekanshi Agrawal <ekanshi@gmail.com>
2018-07-10 12:34:44 +0530

    Re-wrote program to first parse and then evaluate. Also, taking input via REPL

--- lisp_interpreter.js
+++ lisp_interpreter.js
@@ -1,13 +1,14 @@
-let add = (args) => {return args.reduce((a,b) => a+b)}
+
+let add = (...args) => {return args.reduce((a,b) => a+b,0)}
 let sub = (a,b) => a-b
-let mul = (args) => {return args.reduce((a,b) => a*b, 1)}
+let mul = (...args) => {return args.reduce((a,b) => a*b, 1)}
 let div = (a,b) => a/b
 let greater = (a,b) => {return a > b ? true : false}
 let less = (a,b) => {return a < b ? true : false}
 let greaterEqual = (a,b) => {return a >= b ? true : false}
 let lessEqual = (a,b) => {return a <= b ? true : false}
 let equal = (a,b) => {return a == b ? true : false}
-let abs = (args) => {return args.map(Math.abs)}
+let abs = (...args) => {return args.map(Math.abs)}
 
 let env = {
 	'+': add, '-': sub, '*': mul, '/': div,
@@ -15,11 +16,15 @@ let env = {
 	'abs' : abs,
 	'append' : add,
 	'apply' : (proc,args) => proc(...args),
+	'begin' : (...args) => args.pop(),
 	'car' : (args) => args[0],
 	'cdr' : (args) => args.slice(1),
 	'cons' : (x,y) => [x,y],
+	'define' : (symbol, exp) => {env[symbol] = eval(exp)},
+	'eq?' : (x,y) => x === y,
 	'expt' : Math.pow,
 	'equal?' : equal,
+	'if' : (test, conseq, alt) => eval(test) ? eval(conseq) : eval(alt),
 	'length' : (x) => x.length,
 	'list' : (args) => args,
 	'list?' : x => Object.getPrototypeOf(x) == Array.prototype,
@@ -29,37 +34,46 @@ let env = {
 	'not' : x => {if(!x) return T},
 	'null?' : x => x == [],
 	'number?' : x => Object.getPrototypeOf(x) == Number.prototype,
+	'pi' : Math.PI,
 	'print': console.log,
-	'round' : Math.round
+	'procedure' : x => Object.getPrototypeOf(x) == Function.prototype,
+	'round' : Math.round,
+	'symbol?' : x => Object.getPrototypeOf(x) == String.prototype
 }
-//append spaces around ( & ) before sending text after reading file
-function parse(text){
-	let L = []
-	while(text.length != 0){
-		if(text[0] == ')'){
-			let tempL = []
-			let x = L.pop()
-			while(x != '('){
-				tempL.unshift(x)
-				x = L.pop()
-			}
-			if(tempL.length == 1){
-				return tempL[0]
-			}
-			let value = eval(tempL)
-			if(value){
-				L.push(value)
-			}
 
-		}else{
-			let token = text.split(' ')[0]
-			let result = /^\d+(?:\.\d+)?$/.exec(token)
-			L.push(result != null? Number(token) : token)
-		}
-		text = text.split(' ').slice(1).join(' ')
+function parse(text){
+	if(text.startsWith(')')){
+		throw Error("Wrong syntax")
+	}
+	if(text.startsWith('(')){
+		return parseList(text)
+	}else{
+		let token = text.split(' ')[0]
+		let value = /^\d+(?:\.\d+)?$/.exec(token) != null ? Number(token) : token
+		return([value, text.substring(token.length)])
 	}
 }
-console.log(parse('( ( define r 10 ) ( * 3 ( * r r ) ) )'))
+function parseList(text){
+	let parsedText = []
+	text = text.slice(1)
+	let proc = text.split(' ')[0]
+	if(!env.hasOwnProperty(proc)){
+		throw Error("Wrong syntax")
+	}
+	parsedText.push(proc)
+	text = text.split(' ').slice(1).join(' ')
+	while(!text.startsWith(')')){
+		let result = parse(text)
+		parsedText.push(result[0])
+		text = result[1]
+		result = /^(\s+)/.exec(text)
+		if(result){
+			text = text.substring(result[0].length)
+		}
+	}
+	text = text.split(' ').slice(1).join(' ')
+	return [parsedText,text]
+}
 
 function eval(exp){
 	if(Object.getPrototypeOf(exp) == String.prototype){
@@ -67,16 +81,35 @@ function eval(exp){
 	}else if(Object.getPrototypeOf(exp) == Number.prototype){
 		return exp
 	}else if(exp[0] == 'if'){
-		return exp[1] ? exp[2] : exp[3]
+		[_, test, conseq, alt] = exp
+		return eval(test) ? eval(conseq) : eval(alt)
 	}else if (exp[0] == 'define'){
-		env[exp[1]] = exp[2]
+		env[exp[1]] = eval(exp[2])
 		return null
 	}else{
 		let proc = eval(exp[0])
 		let args = exp.slice(1).map(x => eval(x))
-		return proc(args)
+		return proc(...args)		
+		}	
 	}
 
+function repl(){
+	const readLine = require('readline')
+	const r1 = readLine.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		prompt : 'lis.js>'
+	})
+
+	r1.prompt()
+	r1.on('line', (text) => {
+		console.log(text)
+		let parsed = parse(text.replace(/\)/g, ' ) '))
+		let result = eval(parsed[0])
+		if(result != null){
+		console.log(result)
+		}
+		r1.prompt()
+	})
 }
-
-
+repl()
